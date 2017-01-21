ba39e918ef475ccc1f2d9b5bd8fc58d1907651ab
Chris Klanac <chris@thinkful.com>
2017-01-20 19:02:23 -0500

    basic stubs for frontend

--- client/js/actions.js
+++ client/js/actions.js
@@ -1,34 +1,36 @@
 import 'isomorphic-fetch';
 import cookie from 'react-cookie';
 
-export const fetchQuestions = () => dispatch => {
-  dispatch(fetchQuestionsRequest());
-  return fetch('/api/questions', {
-    headers: {
-      'Authorization': `Bearer ${cookie.load('accessToken')}`
-    }
-  }).then(res => {
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
-    return res.json()
-  }).then(data => dispatch(fetchQuestionsSuccess(data)))
-    .catch(err => dispatch(fetchQuestionsError(err)));
-};
-
 export const FETCH_QUESTIONS_REQUEST = 'FETCH_QUESTIONS_REQUEST';
-export const fetchQuestionsRequest = () => ({
-  type: FETCH_QUESTIONS_REQUEST
-});
-
 export const FETCH_QUESTIONS_SUCCESS = 'FETCH_QUESTIONS_SUCCESS';
-export const fetchQuestionsSuccess = questions => ({
-  type: FETCH_QUESTIONS_SUCCESS,
-  questions
-});
-
 export const FETCH_QUESTIONS_ERROR = 'FETCH_QUESTIONS_ERROR';
-export const fetchQuestionsError = error => ({
-  type: FETCH_QUESTIONS_ERROR,
-  error
-});
+
+export function fetchQuestions() {
+  return (dispatch) => {
+    dispatch(fetchQuestionsRequest());
+    return fetch('/api/questions', {
+      headers: {
+        'Authorization': `Bearer ${cookie.load('accessToken')}`
+      }
+    }).then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json()
+    }).then(data => dispatch(fetchQuestionsSuccess(data)))
+      .catch(err => dispatch(fetchQuestionsError(err)));
+  };
+}
+
+export function fetchQuestionsRequest() {
+  return { type: FETCH_QUESTIONS_REQUEST };
+}
+
+export function fetchQuestionsSuccess(questions) {
+  return { type: FETCH_QUESTIONS_SUCCESS, questions };
+}
+
+export function fetchQuestionsError(error) {
+  return { type: FETCH_QUESTIONS_ERROR, error }
+
+};
--- client/js/components/quiz.js
+++ client/js/components/quiz.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchQuestions } from '../actions';
+import * as actions from '../actions';
 
 export class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+  }
   componentDidMount() {
-    this.props.dispatch(fetchQuestions());
+    this.props.dispatch(actions.fetchQuestions());
+  }
+
+  submit(event) {
+    event.preventDefault();
+    this.props.dispatch(actions.submitAnswer({
+      answer: event.target.answer.value
+    }));
+    event.target.reset();
   }
 
   render() {
-    const questions = this.props.questions.map((question, index) => <li key={index}>{question.question} : {question.answer}</li>);
+
+    let current = this.props.questions[0] || {};
+    console.log(this.props.questions);
     return (
       <div>
         <h1>QuiZ</h1>
-        <ul>{questions}</ul>
+        <ul>{current.question}, {current.answer}</ul>
+        <div>
+          <form onSubmit={this.submit}>
+            <input name="answer" type="text" autoComplete="off"></input>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state, props) => ({
-  questions: state.questions
-});
+function mapStateToProps(state, props) {
+  return { questions: state.questions }
+}
 
 export default connect(mapStateToProps)(Quiz);
